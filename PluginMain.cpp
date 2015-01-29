a17fca048a54db75266e66622cb236b2f45be391
Fadekraft <meulengracht@andromeda-it.dk>
2015-01-29 12:40:41 +0100

    Cross-platform CRT method (ish)
    This function takes care of initializing the different plugins
    availiable to the module (dll). It is important for plugin developers to
    add their global instances to this moduel setup function. And the
    function must be present in all modules.

---
+++ PluginMain.cpp
@@ -0,0 +1,18 @@
+/* SpaceTime Plugin Main Function
+ * In this function you setup the needed plugin instances,
+ * they should be global.
+ *
+ */
+
+/* Includes */
+//#include "MyPlugin.h"
+
+/* Instances */
+//static MyPlugin *_MyInstance;
+
+/* Setup */
+extern "C" void ModuleSetup()
+{
+    /* Allocate */
+    //_MyInstance = new MyPlugin();
+}
